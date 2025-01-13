ed7dc484cfdd74047f2bc353fe6bf50799bb15b5
Fubuki <55949664+fubuki-desu@users.noreply.github.com>
2025-01-13 16:35:04 +0800

    implePID.m

--- simplePID.m
+++ simplePID.m
@@ -3,24 +3,24 @@ clc
 % -------------------------- 以放水漏水为例 -------------------------------- 
 h0=0;
 H=5;
-error=H-h0;
-sumError=0;
-error0=0;
-dt=0.1;
-T=1000;
-t=linspace(0,T-1,T);
-h=zeros(1,length(t));
+error=H-h0;% 初始差值
+sumError=0; % 积分
+error0=0; % 前一个差值
+dt=0.1; % 时间间隔
+T=1000; % 总时间
+t=linspace(0,T-1,T); % 时间轴
+h=zeros(1,length(t)); % 位置/水位
 Kp=0.5;
 Ki=0.02;
 Kd=0.02;
-hleak=0.1;
+hleak=0.1;    % 漏水/s
 for i = 2:length(h)
 %     tt=t(i);
-    sumError=sumError+error;
-    error=H-h(i-1);
-    derror=error-error0;
-    h(i)=h(i-1)+Kp*error-hleak*dt+Ki*sumError*dt+Kd*derror/dt;
-    error0=error;
+    sumError=sumError+error;    %积分
+    error=H-h(i-1); % 目标水位-当前水位
+    derror=error-error0; % 差值微分
+    h(i)=h(i-1)+Kp*error-hleak*dt+Ki*sumError*dt+Kd*derror/dt; % 当前水位
+    error0=error; % 赋值
 end
 figure
 plot(t,h)
